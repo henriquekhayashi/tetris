ae7528b39be8c31374df180018695914426fc52f
henriquekhayashi <hayashikenzo@gmail.com>
2019-08-21 11:22:42 -0300

    hi

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define COLUMNS 30
 #define ROWS 10
 
@@ -33,9 +34,27 @@ void printMatrix(char matrix[ROWS][COLUMNS])
 main()
 {
     char matrix [ROWS][COLUMNS];
-
+    int posI, posJ;
+    int a = 1;
+    char b = a;
+    posI = ROWS/2;
+    posJ = COLUMNS/2;
     init(matrix);
-    printMatrix(matrix);
+    while(!kbhit()){
+        system("cls");
 
+    
+    matrix[posI][posJ-1] = ' ';
+    matrix[posI][posJ] = '@';
+
+    printMatrix(matrix);
+    //b = getchar();
+
+    if(posJ<COLUMNS-1) posJ++;
+
+    }
+    
+    
     system("PAUSE");
+    
 }
