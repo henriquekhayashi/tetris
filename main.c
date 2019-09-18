e0bc4f035bc4657fe157e84fe50d5a58f3bd5a56
henriquekhayashi <hayashikenzo@gmail.com>
2019-09-18 11:15:04 -0300

    tecla movimento

--- main.c
+++ main.c
@@ -25,32 +25,79 @@ main()
     posI = ROWS/2;
     posJ = COLUMNS/2;
     init(matrix);
+    int m;
+
+    int tecla;
 
     //apagar o cursor da tela
     ShowConsoleCursor(0);
     system("cls");
-    while(!kbhit()){
+    while(tecla != 27){ /*!kbhit()*/
+        
         
             gotoxy(0,0);
     
     //apaga @
     //matrix[posI][posJ+1] = ' ';
-    matrix[posI-1][posJ] = ' ';
+    //matrix[posI-1][posJ] = ' ';
+    
+
     //escreve@
     matrix[posI][posJ] = '@';
 
     printMatrix(matrix);
-    //b = getchar();
+    printf("%d", m);
+    //b = getch();
     //if(posJ>COLUMNS-COLUMNS) posJ--;
     //walk left
     /*if(posJ>0) posJ--;
     */
+    //matrix[posI][posJ] = ' ';
+    matrix[posI][posJ] = ' ';
+
+    //224
 
    // walk down
     if(posI<ROWS-1) posI++;
-    }
+
     
 
+    if(kbhit()){
+        tecla = getch();
+        /*if(tecla == 'a')
+        posJ--;*/
+        m = tecla;
+    }
+
+  
+  
+
+    switch(tecla){
+        //case 224: 
+            //if(kbhit()) tecla = getch();
+               // switch(tecla){
+                    case 75: 
+                        if(posJ > 0)posJ--;
+                    break;
+                    case 77: 
+                        if(posJ < COLUMNS-1)posJ++;
+                    break;
+                //}
+        //break;
+        case 97: 
+            if(posJ > 0)posJ--;
+        break;
+        case 100: 
+            if(posJ < COLUMNS-1)posJ++;
+        break;
+    }
+
+    //apaga tecla
+    tecla = 0;
+
+    }
+    
+    
     
     system("PAUSE");
     //.c-o.ex start.\pr
--- tetris.h
+++ tetris.h
@@ -9,10 +9,22 @@ data:28/08/2019
 
 #include <stdio.h>
 #include <stdlib.h>
+//#include <conio.h>
 
 //Configuração das dimensoes da matriz principal do jogo
-#define COLUMNS 30
-#define ROWS 10
+#define COLUMNS 60
+#define ROWS 25
+
+//Configuração de teclas
+#define ESC 27
+#define ARROWS 224
+#define LEFT 75
+#define RIGHT 77
+#define TECLA_A 97
+#define TECLA_B 100
+
+
+
 /*
     Inicializa a matriz principal com 'espaços vazios'
 
