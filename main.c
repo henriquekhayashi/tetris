c56832c408fba915722523a70e8a113a9bdc2700
henriquekhayashi <hayashikenzo@gmail.com>
2019-10-09 11:30:59 -0300

    bloco

--- main.c
+++ main.c
@@ -7,10 +7,13 @@ Para exectuar:
 autor:Henrique
 data:28/08/2019
 
+.c-o.ex start.\pr
+.\compile
+-o pr -d debug=1
 */
 
 #include "tetris.h"
-
+#define DEBUG 1
 /*
     Parte principal do programa, responsável por iniciar
     chamar e as funções auxiliares.
@@ -19,11 +22,21 @@ data:28/08/2019
 main()
 {
     char matrix [ROWS][COLUMNS];
-    int posI, posJ;
-    int a = 1;
-    char b = a;
-    posI = ROWS/2;
-    posJ = COLUMNS/2;
+    //int posI, posJ;
+    Bloco tijolo;
+
+    //posição inicial do personagem
+    tijolo.i=0;
+    tijolo.j = COLUMNS/2;
+    tijolo.tipo = TIPO_I;
+    tijolo.orientacao = ORIENTACAO_UP;
+    tijolo.width = 1;
+    tijolo.height = 4;
+
+   // posI = ROWS/2;
+   // posJ = COLUMNS/2;
+   
+    //inicializando matriz
     init(matrix);
     int m;
 
@@ -31,41 +44,80 @@ main()
 
     //apagar o cursor da tela
     ShowConsoleCursor(0);
+   
     system("cls");
-    while(tecla != 27){ /*!kbhit()*/
+   
+   //while(tecla != 27){ /*!kbhit()*/
+    while(1){ /*!kbhit()*/
         
         
             gotoxy(0,0);
+    
+    //print posição do @
+    #if DEBUG == 1
+        printf("@ = (%d,%d)\n", tijolo.i, tijolo.j);
+    #endif
     
     //apaga @
     //matrix[posI][posJ+1] = ' ';
     //matrix[posI-1][posJ] = ' ';
     
 
-    //escreve@
-    matrix[posI][posJ] = '@';
 
+    //escreve@
+    switch(tijolo.orientacao){
+    case ORIENTACAO_UP:
+        matrix[tijolo.i][tijolo.j] = PIXEL;
+        if(tijolo.i-1>=0) matrix[tijolo.i-1][tijolo.j] = PIXEL;
+        if(tijolo.i-2>=0) matrix[tijolo.i-2][tijolo.j] = PIXEL;
+        if(tijolo.i-3>=0) matrix[tijolo.i-3][tijolo.j] = PIXEL;
+        break;
+    case ORIENTACAO_LEFT:
+        matrix[tijolo.i][tijolo.j] = PIXEL;
+        if(tijolo.i-1>=0) matrix[tijolo.i][tijolo.j-1] = PIXEL;
+        if(tijolo.i-2>=0) matrix[tijolo.i][tijolo.j-2] = PIXEL;
+        if(tijolo.i-3>=0) matrix[tijolo.i][tijolo.j-3] = PIXEL;
+        //FUNCAO DESENHAR
+        break;
+    }
     printMatrix(matrix);
-    printf("%d", m);
+   
+   //print tecla
+    //printf("%d", m);
+   
     //b = getch();
     //if(posJ>COLUMNS-COLUMNS) posJ--;
     //walk left
     /*if(posJ>0) posJ--;
     */
     //matrix[posI][posJ] = ' ';
-    matrix[posI][posJ] = ' ';
 
+    switch(tijolo.orientacao){
+    case ORIENTACAO_UP:
+    matrix[tijolo.i][tijolo.j] = EMPTY;
+    if(tijolo.i-1>=0) matrix[tijolo.i-1][tijolo.j] = EMPTY;
+    if(tijolo.i-2>=0) matrix[tijolo.i-2][tijolo.j] = EMPTY;
+    if(tijolo.i-3>=0) matrix[tijolo.i-3][tijolo.j] = EMPTY;
+        break;
+    case ORIENTACAO_LEFT:
+        matrix[tijolo.i][tijolo.j] = EMPTY;
+        if(tijolo.i-1>=0) matrix[tijolo.i][tijolo.j-1] = EMPTY;
+        if(tijolo.i-2>=0) matrix[tijolo.i][tijolo.j-2] = EMPTY;
+        if(tijolo.i-3>=0) matrix[tijolo.i][tijolo.j-3] = EMPTY;
+        break;
+    }
     //224
 
    // walk down
-    if(posI<ROWS-1) posI++;
+    if(tijolo.i<ROWS-1) tijolo.i++;
 
     
 
     if(kbhit()){
         tecla = getch();
        
-        m = tecla;
+       //print tecla
+        //m = tecla;
     }
 
   
@@ -80,20 +132,27 @@ main()
             if(kbhit()) tecla = getch();
                 switch(tecla){
                     case 75: 
-                        if(posJ > 0)posJ--;
+                        if(tijolo.j > 0)tijolo.j--;
                     break;
                     case 77: 
-                        if(posJ < COLUMNS-1)posJ++;
+                        if(tijolo.j < COLUMNS-1)tijolo.j++;
                     break;
                 }
         break;
         case 97: 
-            if(posJ > 0)posJ--;
+            if(tijolo.j > 0)tijolo.j--;
         break;
         case 100: 
-            if(posJ < COLUMNS-1)posJ++;
+            if(tijolo.j < COLUMNS-1)tijolo.j++;
+        case 114:
+
+            if(tijolo.orientacao == ORIENTACAO_UP)tijolo.orientacao = ORIENTACAO_LEFT;
+            //if(tijolo.orientacao == ORIENTACAO_LEFT)tijolo.orientacao = ORIENTACAO_UP;
         break;
     }
+
+    // sair com esc
+    if (tecla == 27) break;
 
     //apaga tecla
     tecla = 0;
@@ -103,7 +162,7 @@ main()
     
     
     system("PAUSE");
-    //.c-o.ex start.\pr
+    
 }
 
 
--- tetris.h
+++ tetris.h
@@ -23,6 +23,36 @@ data:28/08/2019
 #define TECLA_A 97
 #define TECLA_B 100
 
+//Criando as orientações das peças
+#define ORIENTACAO_UP 1
+#define ORIENTACAO_DOWN 2
+#define ORIENTACAO_LEFT 3
+#define ORIENTACAO_RIGHT 4
+
+//Criando os tipos de peças
+#define TIPO_L 1
+#define TIPO_L_R 2 //L reverso
+#define TIPO_T 3
+#define TIPO_Z 4
+#define TIPO_Z_R 5 //Z reverso
+#define TIPO_O 6 //Quadrado
+#define TIPO_I 7
+
+//Configurações de layou
+#define PIXEL 219
+
+#define EMPTY 32
+
+//Estrutura padrão de componentes
+typedef struct{
+    int i; //posição linhas da matriz
+    int j; //posição colunas matriz
+    int orientacao; //orientação da peça
+    int tipo; // o tipo de 7 possiveis
+
+    int width; //largura
+    int height; //altura
+}Bloco;
 
 
 /*
